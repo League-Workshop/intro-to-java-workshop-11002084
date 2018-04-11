09374e25a63024d8e91d01c4fd1b0e18b879c4ab
11002084 <league@WTS7.attlocal.net>
2018-04-11 14:52:50 -0700

    Day 3
    I made a java that greets people and a java that says things according
    to what people type.

---
+++ src/section3/Compliments.java
@@ -0,0 +1,16 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Compliments {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null,"I want to play a game. If you keep this text box, it will annoy for all eternity. If you click ok or shut off your computer, it will activate a deadly computer virus that can hack your computer in seconds. Choose now.");
+		JOptionPane.showMessageDialog(null, "You clicked ok! You idiot! Your computer has now been hacked!");
+		JOptionPane.showMessageDialog(null, "Searching for bank account...");
+		JOptionPane.showMessageDialog(null, "Bank account located.");
+		JOptionPane.showMessageDialog(null, "Draining bank account...");
+		JOptionPane.showMessageDialog(null, "Bank account drained.");
+		JOptionPane.showMessageDialog(null, "Thanks for the free money.");
+		JOptionPane.showMessageDialog(null, "BYE!!!!!!!!!!!");
+	}
+}
